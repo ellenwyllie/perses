d005ed77070ecb2db3f2ef54fec673a686af18dc
Steven Cobb <sjcobb.dev@gmail.com>
2023-02-17 10:00:21 -0500

    add visual options panel to be used in visual regression test, mock values todo
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/e2e/src/tests/timeSeriesChartPanel.spec.ts
+++ ui/e2e/src/tests/timeSeriesChartPanel.spec.ts
@@ -69,4 +69,119 @@ test.describe('Dashboard: Time Series Chart Panel', () => {
       });
     });
   });
+
+  test(`displays time series with custom visual and y_axis options`, async ({ page, dashboardPage, mockNow }) => {
+    // Mock data response, so we can make assertions on consistent response data.
+    await dashboardPage.mockQueryRangeRequests({
+      queries: [
+        {
+          query: 'node_load15{instance="demo.do.prometheus.io:9100",job="node"}',
+          response: {
+            status: 200,
+            body: JSON.stringify(
+              mockTimeSeriesResponseWithStableValue({
+                metrics: [
+                  {
+                    metric: {
+                      __name__: 'node_load15',
+                      env: 'demo',
+                      instance: 'demo.do.prometheus.io:9100',
+                      job: 'node',
+                    },
+                    values: [
+                      [1676643893.128, '3.55'],
+                      [1676643894.128, '3.55'],
+                      [1676643895.128, '3.55'],
+                      [1676643896.128, '3.55'],
+                      [1676643897.128, '3.55'],
+                      [1676643898.128, '3.55'],
+                      [1676643899.128, '3.51'],
+                      [1676643900.128, '3.51'],
+                      [1676643901.128, '3.51'],
+                      [1676643902.128, '3.51'],
+                      [1676643903.128, '3.51'],
+                      [1676643904.128, '3.51'],
+                      [1676643905.128, '3.51'],
+                      [1676643906.128, '3.51'],
+                      [1676643907.128, '3.51'],
+                      [1676643908.128, '3.51'],
+                      [1676643909.128, '3.51'],
+                      [1676643910.128, '3.51'],
+                      [1676643911.128, '3.51'],
+                      [1676643912.128, '3.51'],
+                      [1676643913.128, '3.51'],
+                      [1676643914.128, '3.45'],
+                      [1676643915.128, '3.45'],
+                      [1676643916.128, '3.45'],
+                      [1676643917.128, '3.45'],
+                      [1676643918.128, '3.45'],
+                      [1676643919.128, '3.45'],
+                      [1676643920.128, '3.45'],
+                      [1676643921.128, '3.45'],
+                      [1676643922.128, '3.45'],
+                      [1676643923.128, '3.45'],
+                      [1676643924.128, '3.45'],
+                      [1676643925.128, '3.45'],
+                      [1676643926.128, '3.45'],
+                      [1676643927.128, '3.45'],
+                      [1676643928.128, '3.45'],
+                      [1676643929.128, '3.39'],
+                      [1676643930.128, '3.39'],
+                      [1676643931.128, '3.39'],
+                      [1676643932.128, '3.39'],
+                      [1676643933.128, '3.39'],
+                      [1676643934.128, '3.39'],
+                      [1676643935.128, '3.39'],
+                      [1676643936.128, '3.39'],
+                      [1676643937.128, '3.39'],
+                      [1676643938.128, '3.39'],
+                      [1676643939.128, '3.39'],
+                      [1676643940.128, '3.39'],
+                      [1676643941.128, '3.39'],
+                      [1676643942.128, '3.39'],
+                      [1676643943.128, '3.39'],
+                      [1676643944.128, '3.34'],
+                      [1676643945.128, '3.34'],
+                      [1676643946.128, '3.34'],
+                      [1676643947.128, '3.34'],
+                      [1676643948.128, '3.34'],
+                      [1676643949.128, '3.34'],
+                      [1676643950.128, '3.34'],
+                      [1676643951.128, '3.34'],
+                      [1676643952.128, '3.34'],
+                      [1676643953.128, '3.34'],
+                    ],
+                  },
+                ],
+                startTimeMs: mockNow - 6 * 60 * 60 * 1000,
+                endTimeMs: mockNow,
+              })
+            ),
+          },
+        },
+      ],
+    });
+
+    await dashboardPage.forEachTheme(async (themeName) => {
+      const panel = dashboardPage.getPanelByName('Single Line');
+      await panel.isLoaded();
+      await waitForStableCanvas(panel.canvas);
+
+      await happoPlaywright.screenshot(page, panel.parent, {
+        component: 'Time Series Chart Panel',
+        variant: `Single Line [${themeName}]`,
+      });
+    });
+
+    // await dashboardPage.forEachTheme(async (themeName) => {
+    //   const panel = dashboardPage.getPanelByName('Line Visual Options');
+    //   await panel.isLoaded();
+    //   await waitForStableCanvas(panel.canvas);
+
+    //   await happoPlaywright.screenshot(page, panel.parent, {
+    //     component: 'Time Series Chart Panel',
+    //     variant: `Line Visual Options [${themeName}]`,
+    //   });
+    // });
+  });
 });
