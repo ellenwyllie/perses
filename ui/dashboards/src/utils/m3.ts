691318a3b31a35fc14a9c3ae56d2f6e095d940c8
Steven Cobb <sjcobb.dev@gmail.com>
2023-03-10 11:12:40 -0500

    cleanup createM3Client, add processInstantQueryResponse
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/dashboards/src/utils/m3.ts
+++ ui/dashboards/src/utils/m3.ts
@@ -12,17 +12,7 @@
 // limitations under the License.
 
 import { RequestHeaders } from '@perses-dev/core';
-// import { ApiResponse, PrometheusClient, RangeQueryResponse, SuccessResponse } from '@perses-dev/prometheus-plugin';
-import {
-  InstantQueryResponse,
-  RangeQueryResponse,
-  PrometheusClient,
-  // PrometheusDatasourceSpec,
-  InstantQueryRequestParameters,
-  LabelNamesRequestParameters,
-  LabelValuesRequestParameters,
-  RangeQueryRequestParameters,
-} from '@perses-dev/prometheus-plugin';
+import { InstantQueryResponse, RangeQueryResponse, PrometheusClient } from '@perses-dev/prometheus-plugin';
 import { DatasourceClient } from '@perses-dev/plugin-system';
 
 export function createM3Client(client: DatasourceClient): DatasourceClient {
@@ -31,6 +21,7 @@ export function createM3Client(client: DatasourceClient): DatasourceClient {
 }
 
 function transformPrometheusClient(client: DatasourceClient): PrometheusClient {
+  // TODO: validate Prom client
   const promClient = client as PrometheusClient;
 
   // custom query limit HTTP headers
@@ -43,52 +34,22 @@ function transformPrometheusClient(client: DatasourceClient): PrometheusClient {
     options: {
       datasourceUrl: promClient.options.datasourceUrl,
     },
-    instantQuery: (params: InstantQueryRequestParameters) => {
-      return promClient.instantQuery(params, headers).then(addInstantQueryWarnings);
+    instantQuery: (params) => {
+      return promClient.instantQuery(params, headers).then(processInstantQueryResponse);
     },
-    rangeQuery: (params: RangeQueryRequestParameters) => {
+    rangeQuery: (params) => {
       return promClient.rangeQuery(params, headers).then(processRangeQueryResponse);
     },
-    labelNames: (params: LabelNamesRequestParameters) => {
+    labelNames: (params) => {
       const result = promClient.labelNames(params);
       return result;
     },
-    labelValues: (params: LabelValuesRequestParameters) => {
+    labelValues: (params) => {
       const result = promClient.labelValues(params);
       return result;
     },
   };
 }
-
-// export function createOldM3Client(realClient: PrometheusClient): PrometheusClient {
-//   // custom query limit HTTP headers
-//   const headers: RequestHeaders = {
-//     'm3-limit-max-returned-datapoints': '56',
-//     'm3-limit-max-returned-series': '2',
-//   };
-
-//   const client: PrometheusClient = {
-//     options: {
-//       datasourceUrl: realClient.options.datasourceUrl,
-//     },
-//     instantQuery: (params) => {
-//       return realClient.instantQuery(params, headers).then(addInstantQueryWarnings);
-//     },
-//     rangeQuery: (params) => {
-//       // return realClient.rangeQuery(params, headers).then(addRangeQueryWarnings);
-//       return realClient.rangeQuery(params, headers).then(processRangeQueryResponse);
-//     },
-//     labelNames: (params) => {
-//       const result = realClient.labelNames(params);
-//       return result;
-//     },
-//     labelValues: (params) => {
-//       const result = realClient.labelValues(params);
-//       return result;
-//     },
-//   };
-//   return client;
-// }
 
 function processRangeQueryResponse(results: RangeQueryResponse): Promise<RangeQueryResponse> {
   if (results.status !== 'success') {
@@ -101,38 +62,13 @@ function processRangeQueryResponse(results: RangeQueryResponse): Promise<RangeQu
   return Promise.resolve(results);
 }
 
-// // TODO: test error handling, make more generic, should this be ApiResponse<T> or SuccessResponse<T>
-// // function addWarnings(results: SuccessResponse): Promise<SuccessResponse> {
-// function addRangeQueryWarnings(results: RangeQueryResponse): Promise<RangeQueryResponse> {
-//   if (results.status === 'success') {
-//     if (results.headers !== undefined) {
-//       const entries = [...results.headers.entries()];
-//       const entry = entries.find((entry) => entry[0] === 'm3-returned-data-limited');
-//       const warning = entry?.[1];
-//       if (warning !== undefined) {
-//         results.warnings = [warning];
-//       }
-//     }
-//   }
-//   return Promise.resolve(results);
-// }
-
-function addInstantQueryWarnings(results: InstantQueryResponse): Promise<InstantQueryResponse> {
-  if (results.status === 'success') {
-    results.warnings = ['M3', 'INSTANT_WARNINGS'];
+function processInstantQueryResponse(results: InstantQueryResponse): Promise<InstantQueryResponse> {
+  if (results.status !== 'success') {
+    return Promise.resolve(results);
+  }
+  const warning = results.headers?.get('m3-returned-data-limited') ?? undefined;
+  if (warning !== undefined) {
+    results.warnings = [warning];
   }
   return Promise.resolve(results);
-}
-
-/**
- * Fetch JSON and parse warnings for query inspector
- */
-export async function fetchResults<T>(...args: Parameters<typeof global.fetch>) {
-  const response = await fetch(...args);
-  const json: T = await response.json();
-  const entries = [...response.headers.entries()];
-  // const entry = entries.find((entry) => entry[0] === warnings_header);
-  const entry = entries.find((entry) => entry[0] === 'Test');
-  const warning = entry?.[1];
-  return { ...json, warnings: [warning], headers: response.headers };
 }
